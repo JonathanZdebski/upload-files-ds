9e020053e4796762d2bc35f1353eb6ca826727d9
JonathanZdebski <jonathanzdebski@yahoo.com>
2024-08-25 15:49:56 -0300

    Seo Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,17 +31,7 @@ export default function RootLayout({
           name="google-site-verification"
           content="ZdDdkr8rensIcRxSdhlzsbtRReJ4EXa_MjkoORGrkxk"
         />
-        <Script
-          id="gtm"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PS2PC45L');`,
-          }}
-        ></Script>
+        <Script></Script>
       </head>
       <body className={inter.className}>
         <SessionProvider>
