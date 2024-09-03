68e348008bb38ec0399de9f1afa345eb3a0f6717
JonathanZdebski <jonathanzdebski@yahoo.com>
2024-09-03 16:33:43 -0300

    NavbarFix

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -13,6 +13,9 @@ const Navbar = () => {
   const { data: session } = useSession(); // Obtendo dados da sessão
   const [isOpen, setIsOpen] = useState(false); // Estado para controle do menu
 
+  // Função para fechar o menu
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <nav className="bg-transparent text-white w-full p-4 flex justify-between items-center relative">
       <div className="flex items-center">
@@ -24,6 +27,7 @@ const Navbar = () => {
 
       {/* Botão de menu para dispositivos móveis */}
       <button
+        aria-label="Toggle navigation menu"
         className="block md:hidden px-3 py-2 border border-gray-300 rounded text-gray-300 hover:text-white hover:border-white focus:outline-none"
         onClick={() => setIsOpen(!isOpen)}
       >
@@ -69,7 +73,7 @@ const Navbar = () => {
           <li className="bg-blue-600 bg-opacity-30 p-2 rounded-lg transition-colors duration-200 hover:bg-blue-600 hover:bg-opacity-50">
             <Link
               href="/login"
-              className="text-blue-300 hover:bg-opacity-40 px-2 py-2 rounded transition-colors duration-200"
+              className="text-blue-400 hover:bg-opacity-40 px-2 py-2 rounded transition-colors duration-200"
             >
               Login
             </Link>
@@ -89,20 +93,21 @@ const Navbar = () => {
 
       {/* Menu de navegação para dispositivos móveis */}
       <div
-        className={`fixed top-0 left-0 w-full h-screen bg-gray-900 bg-opacity-70 backdrop-blur-md transform${
+        className={`fixed top-0 left-0 w-full h-screen bg-gray-900 bg-opacity-70 backdrop-blur-md transform ${
           isOpen ? "translate-x-0" : "translate-x-full"
         } transition-transform duration-300 ease-in-out z-50 md:hidden`}
-        onClick={() => setIsOpen(false)}
+        onClick={closeMenu} // Fecha o menu ao clicar fora dele
       >
         <ul
           className="list-none flex flex-col space-y-4 p-4 text-center mt-32"
-          onClick={(e) => e.stopPropagation()}
+          onClick={(e) => e.stopPropagation()} // Evita que o clique no menu feche-o
         >
           <li className="relative group">
             <span className="absolute inset-0 bg-slate-600 bg-opacity-25 rounded w-3/4 left-1/2 transform -translate-x-1/2 h-full"></span>
             <Link
               href="/"
               className="relative text-white hover:text-gray-300 z-10 p-2"
+              onClick={closeMenu} // Fecha o menu ao clicar em um item
             >
               Home
             </Link>
@@ -111,14 +116,22 @@ const Navbar = () => {
 
           <li className="relative group">
             <span className="absolute inset-0 bg-slate-600 bg-opacity-25 rounded w-3/4 left-1/2 transform -translate-x-1/2 h-full"></span>
-            <Link href="/about" className="hover:text-gray-300">
+            <Link
+              href="/about"
+              className="hover:text-gray-300"
+              onClick={closeMenu} // Fecha o menu ao clicar em um item
+            >
               About
             </Link>
             <span className="absolute bottom-[-3px] left-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-200 origin-left"></span>
           </li>
           <li className="relative group">
             <span className="absolute inset-0 bg-slate-600 bg-opacity-25 rounded w-3/4 left-1/2 transform -translate-x-1/2 h-full"></span>
-            <Link href="/contact" className="hover:text-gray-300">
+            <Link
+              href="/contact"
+              className="hover:text-gray-300"
+              onClick={closeMenu} // Fecha o menu ao clicar em um item
+            >
               Contact
             </Link>
             <span className="absolute bottom-[-3px] left-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
@@ -130,6 +143,7 @@ const Navbar = () => {
               <Link
                 href="/login"
                 className="text-blue-400 hover:bg-opacity-40 px-2 py-2 rounded transition-colors duration-200"
+                onClick={closeMenu} // Fecha o menu ao clicar em um item
               >
                 Login
               </Link>
